a1dad531fa5095d85810b1f6b67bc5cf5346ae6e
mbroons <mbrooons@gmail.com>
2017-12-12 16:28:17 +0100

    floor key ? ceiling key ?

---
+++ src/test/java/com/biffbangpow/shift/TreeMapTest.java
@@ -0,0 +1,37 @@
+package com.biffbangpow.shift;
+
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.lang.*;
+import java.lang.Integer;
+import java.util.TreeMap;
+
+public class TreeMapTest {
+
+    private TreeMap<java.lang.Integer, String> map = new TreeMap<>();
+
+    @BeforeClass
+    public void setUp() {
+        map.put(1, "1");
+        map.put(4, "1");
+        map.put(5, "1");
+        map.put(7, "1");
+        map.put(8, "1");
+        map.put(9, "1");
+    }
+
+    @Test
+    public void test_get_floor_key() {
+
+        Assert.assertEquals((int) map.floorKey(6), 5);
+    }
+
+    @Test
+    public void test_get_ceiling_key() {
+
+        Assert.assertEquals((int) map.ceilingKey(6), 7);
+    }
+}

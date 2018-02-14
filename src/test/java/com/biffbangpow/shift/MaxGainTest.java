567784c79f35a650d579d0b3f7e424518f0de1d7
mbroons <mbrooons@gmail.com>
2018-02-14 20:52:41 +0100

    added test_is_null in master

--- src/test/java/com/biffbangpow/shift/MaxGainTest.java
+++ src/test/java/com/biffbangpow/shift/MaxGainTest.java
@@ -12,4 +12,10 @@ public class MaxGainTest {
         int[] prices = {2, 3, 4, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 6, 5};
         Assert.assertEquals(MaxGain.maxGain(prices), new MaxGain.Result(7, 13, 6));
     }
+
+    @Test
+    public void test_isnull() {
+
+        Assert.assertNull(null);
+    }
 }
